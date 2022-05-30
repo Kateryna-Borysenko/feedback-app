add4205b7e21486a4e23bc872175b1bf44315fcb
Kateryna <k.borysenko.kyiv@gmail.com>
2022-05-30 19:33:10 +0300

    Added average rating

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -2,7 +2,7 @@ import FeedbackItem from './FeedbackItem'
 import PropTypes from 'prop-types'
 
 function FeedbackList({ feedback, handleDelete }) {
-  console.log('🍒 feedback', feedback)
+  // console.log('🍒 feedback', feedback)
   if (!feedback || feedback.length === 0) {
     return <p>No Feedback Yet</p>
   }
