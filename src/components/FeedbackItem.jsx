71955239498315f2eb9830664f7ed0421cf55165
Kateryna <k.borysenko.kyiv@gmail.com>
2022-06-02 18:47:47 +0300

    Added useContext

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,16 +1,22 @@
-import { FaTimes } from 'react-icons/fa'
+import { FaTimes, FaEdit } from 'react-icons/fa'
+import { useContext } from 'react'
 import Card from './shared/Card'
 import PropTypes from 'prop-types'
+import FeedbackContext from '../context/FeedbackContext'
 
-function FeedbackItem({ item, handleDelete }) {
+function FeedbackItem({ item }) {
+  const { deleteFeedback } = useContext(FeedbackContext)
   const { rating, text, id } = item
 
   return (
     <Card>
       <div className='num-display'>{rating}</div>
-      <button onClick={() => handleDelete(id)} className='close'>
+      <button onClick={() => deleteFeedback(id)} className='close'>
         <FaTimes color='purple' />
       </button>
+      {/* <button onClick={() => editFeedback(item)} className='edit'>
+        <FaEdit color='purple' />
+      </button> */}
       <div className='text-display'>{text}</div>
     </Card>
   )
